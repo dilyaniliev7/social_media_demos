5652faa455cab7a56e74509463e83e29bbfc8f99
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-05-08 15:00:14 +0300

    API End point & load data to profile

--- backend/frontend/src/routes/user_profile.js
+++ backend/frontend/src/routes/user_profile.js
@@ -1,5 +1,8 @@
 import { Text, VStack, Flex, Box, Heading, HStack, Image, Button } from "@chakra-ui/react"
 import { useState, useEffect } from 'react'
+import { get_user_profile_data } from '../api/endpoints';
+import { SERVER_URL } from '../constants/constants';
+
 const UserProfile = () => {
 
     const get_username_from_url = () => {
@@ -23,28 +26,55 @@ const UserProfile = () => {
 }
 
 const UserDetails = ({username}) => {
+
+    const [loading, setLoading] = useState(true)
+    const [bio, setBio] = useState('')
+    const [profileImage, setProfileImage] = useState('')
+    const [followerCount, setFollowerCount] = useState(0)
+    const [followingCount, setFollowingCount] = useState(0)
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const data = await get_user_profile_data(username);
+                setBio(data.bio)
+                setProfileImage(data.profile_image)
+                setFollowerCount(data.follower_count)
+                setFollowingCount(data.following_count)
+            } catch {
+                console.log('error')
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+
+    },[])
+
     return (
         <VStack w='100%' alignItems='start' gap='40px'>
             <Heading>@{username}</Heading>
             <HStack gap='20px'>
                 <Box boxSize='150px' border='2px solid' borderColor='gray.700' bg='white' borderRadius='full' overflow='hidden'>
-                    <Image src="http://127.0.0.1:8000/api/media/profile_image/" boxSize='100%' objectFit='cover'/>
+                    <Image src={ loading ? '' : `${SERVER_URL}${profileImage}`} boxSize='100%' objectFit='cover'/>
                 </Box>
                 <VStack gap='20px'>
                     <HStack gap='20px' fontSize='18px'>
                         <VStack>
                             <Text>Followers</Text>
-                            <Text>0</Text>
+                            <Text>{ loading ? '-' : followerCount}</Text>
                         </VStack>
                         <VStack>
                             <Text>Following</Text>
-                            <Text>0</Text>
+                            <Text>{ loading ? '-' : followingCount}</Text>
                         </VStack>
                     </HStack>
                     <Button w='100%'>Edit Profile</Button>
                 </VStack>
             </HStack>
-            <Text fontSize='18px'>Hi its dilyaniliev</Text>
+            <Text fontSize='18px'>{ loading ? '-' : bio}</Text>
         </VStack>
     )
 }
