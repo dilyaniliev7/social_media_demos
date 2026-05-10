7775506765df9c37ec051b92d15b8bf4c253316c
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-05-10 19:10:20 +0300

    React Login Form created

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import UserProfile from './routes/user_profile';
 import Layout from './components/layout'
-
+import Login from './routes/login';
 function App() {
   return (
       <ChakraProvider>
         <Router>
             <Routes>
                 <Route element={<Layout><UserProfile/></Layout>} path='/:username'/>
+                <Route element={<Layout><Login/></Layout>} path='/login'/>
             </Routes>
         </Router>
       </ChakraProvider>
