8a9b829c137a73eea86990afdc7d08cc9043133a
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-05-14 18:34:02 +0300

    post serializer & model created

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MyUser
+from .models import MyUser, Post
 
 class UserRegisterSerializer(serializers.ModelSerializer):
 
@@ -35,3 +35,22 @@ class MyUserProfileSerializer(serializers.ModelSerializer):
 
     def get_following_count(self, obj):
         return obj.following.count()
+
+class PostSerializer(serializers.ModelSerializer):
+
+    username = serializers.SerializerMethodField()
+    like_count = serializers.SerializerMethodField()
+    formatted_date = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['username', 'description', 'formatted_date', 'likes', 'like_count']
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_like_count(self, obj):
+        return obj.likes.count()
+
+    def get_formatted_date(self, obj):
+        return obj.created_at.strftime("%d %b %y")
