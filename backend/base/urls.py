e8a63cb15cab8d9ba981806c8609189a2ba1d927
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-05-18 20:29:25 +0300

    get posts & paginator added

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import get_user_profile_data, CustomTokenObtainPairView, CustomTokenRefreshView, register, authenticated, toggleFollow, get_users_posts, toggleLike, create_post
+from .views import get_user_profile_data, CustomTokenObtainPairView, CustomTokenRefreshView, register, authenticated, toggleFollow, get_users_posts, toggleLike, create_post, get_posts
 
 
 urlpatterns = [
@@ -15,4 +15,5 @@ urlpatterns = [
     path('posts/str:pk>/', get_users_posts),
     path('toggleLike', toggleLike),
     path('create_post', create_post),
+    path('get_posts/', get_posts),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from rest_framework.pagination import PageNumberPagination
+
 from .models import MyUser, Post
 from .serializers import MyUserProfileSerializer, UserRegisterSerializer, PostSerializer
 
@@ -201,3 +203,35 @@ def create_post(request):
         return Response(serializer.data)
     except:
         return Response({"error":"error creating post"})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_posts(request):
+    try:
+        user = MyUser.objects.get(username=pk)
+        my_user = MyUser.objects.get(username=request.user.username)
+    except MyUser.DoesNotExist:
+        return Response({'error':'user does not exist'})
+
+    posts = Post.objects.all().order_by('-created_at')
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+
+    result_page = paginator.paginate_queryset(posts, request)
+
+    serializer = PostSerializer(result_page, many=True)
+
+    data = []
+
+    for post in serializer.data:
+        new_post = {}
+
+        if my_user.username in post['likes']:
+            new_post = {**post, 'liked':True}
+        else:
+            new_post = {**post, 'liked':False}
+
+        data.append(new_post)
+    return paginator.get_paginated_response(data)
